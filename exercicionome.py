e025615ad6fcef5a98d949689875a3a34b110348
victorhugooliveiraa <146842052+victorhugooliveiraa@users.noreply.github.com>
2024-01-16 11:05:10 -0300

    Add files via upload

---
+++ exercicionome.py
@@ -0,0 +1,15 @@
+nome = input('Digite seu nome: ')
+idade = input('Digite sua idade: ')
+
+if nome and idade :
+    print(f'O seu nome é {nome}')
+    print(f'Seu nome invertido é {nome[::-1]}')
+    if ' ' in nome:
+        print('Seu nome contém (ou não) espaços')
+    else:
+        print('Seu nome NÃO contém espaços.')
+
+
+print(f'Seu nome tem {len(nome)} letras')
+print(f'A primeira letra do seu nome é {nome[0]}')
+print(f'E a ultima letra do seu nome é {nome[5]}')
